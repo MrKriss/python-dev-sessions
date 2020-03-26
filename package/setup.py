4a049eb5670b30e8e25b0cc8260502e298a118e4
Chris Musselle <Chris.Musselle@malvernpanalytical.com>
2020-03-26 16:32:30 +0000

    Make use of find_packages

--- package/setup.py
+++ package/setup.py
@@ -6,12 +6,12 @@ setup(
     # The list of python packages, relative to setup.py to include.
     # Note that find_packages is useful here when you have lots of sub packages
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
-    packages=["mpg"],
+    packages=find_packages("mpg"),
     # Version String, please use semantic versioning https://semver.org/
     version="0.1.0",
     # Dependencies that will be installed by pip before this package.
     install_requires=["pandas", "matplotlib"],
     package_data={
         "mpg": ["*.json"],
-    }
+    },
 )
